8d4b7dd989cd22e9aaa901bd185216713ef66912
Daniel Hoffmann <103540391+Pepa90210@users.noreply.github.com>
2022-08-11 11:04:04 -0500

    clean

--- src/context/PupContext.js
+++ src/context/PupContext.js
@@ -2,18 +2,19 @@
 import { createContext, useState, useContext } from "react";
 import speedy from "../pups/speedy-pup.jpg";
 
-export const PupContext = createContext();
+const PupContext = createContext();
+
+export function usePuppyType() {
+  return useContext(PupContext);
+}
 
 export function PupProvider(props) {
   const [puppyType, setPuppyType] = useState(speedy);
+  // console.log(PupContext)
 
   return (
     <PupContext.Provider value={{ puppyType, setPuppyType }}>
       {props.children}
     </PupContext.Provider>
   );
-}
-
-export function usePuppyType() {
-  return useContext(PupContext);
 }
